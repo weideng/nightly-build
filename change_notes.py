8a6e2906d8df94de41430a19217b7965dda4b653
tiger <tiger@Tiger-PC.(none)>
2012-11-02 16:37:55 +0800

    modify scripts

--- change_notes.py
+++ change_notes.py
@@ -38,17 +38,19 @@ def find_rev(path):
     for HEAD and return it as a string"""
     # Should verify that rev-parse will only ever print the rev to stdout
     return git_op(['git', 'rev-parse', 'HEAD'], path=path)
-
+def get_diffs(path):
+    return git_op(['git', '', ''], path=path)
 def main():
     #today = date.today()
     #yesterday = date.today() - timedelta(days=1)
-    file_path1 = os.getcwd() + "/nightly/otoro/12-10-31/manifest.xml"
-    file_path2 = os.getcwd() + "/nightly/otoro/12-11-01/manifest.xml"
+    file_path1 = os.getcwd() + "/otoro/12-10-31/manifest.xml"
+    file_path2 = os.getcwd() + "/otoro/12-11-01/manifest.xml"
     doc1 = xml.dom.minidom.parse(file_path1)
     doc2 = xml.dom.minidom.parse(file_path2)
     dic1 = {}
     dic2 = {}
     log = ''
+
     for project in doc1.getElementsByTagName("project"):
         manifest_path = project.getAttribute('path')
         commit_id = project.getAttribute('revision')
@@ -57,18 +59,33 @@ def main():
         manifest_path = project.getAttribute('path')
         commit_id = project.getAttribute('revision')
         dic2[manifest_path] = commit_id
+
     keys1 = dic1.keys()
     keys2 = dic2.keys()
     s1 = set(keys1)
     s2 = set(keys2)
-    print s2.difference(s1)
-    for index in range(len(keys1)):
-        if keys1[index] in keys2:
-            #print keys1[index]
-            pass
-        else:
-            log += 'repository ' + keys1[index] + ' has been removed\n'
-           # print log
+
+    s3 = s1.difference(s2)
+    if (len(s3) > 0):
+        for k in s3: 
+            log += 'Repository ' + k + ' has been removed.\n'
+
+    s3 = s2.difference(s1)
+    if (len(s3) > 0):
+        for k in s3:
+            log += 'Repository ' + k + ' has been added.\n'
+
+    s3 = s1.intersection(s2)
+    if (len(s3) > 0):
+        for k in s3:
+            if dic1[k] != dic2[k]:
+                log += 'Repository ' + k + ' has been changed\n'
+	        path = k.replace('/','_')
+                fs_path = os.path.join('/home/tiger/work/B2G-otoro/',k)
+                #print get_diffs(fs_path)
+    file_object = open('project-change-info.log','w')
+    file_object.write(log)
+    file_object.close()
       #  fs_path = os.path.join(root_path, manifest_path)
       #  commit_id = find_rev(fs_path)
       #  project.setAttribute('revision', commit_id)
