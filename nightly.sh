8410967c88a9e563ffc276dcc3d4b186fe8d649f
wdeng <wdeng@mozilla.com>
2012-11-08 18:05:37 +0800

    rename nightly script

--- nightly.sh
+++ nightly.sh
@@ -4,8 +4,8 @@ TODAY=`date +%Y-%m-%d`
 YESTERDAY=`date -d yesterday +%Y-%m-%d`
 CURRENT_DIR=`pwd`
 
-OTORO_SRC="/home/wdeng/work/B2G-otoro"
-SP8810EA_SRC="/home/wdeng/work/B2G-sp8810ea"
+OTORO_SRC="/home/wdeng/work/b2g-otoro"
+SP8810EA_SRC="/home/wdeng/work/b2g-sp8810ea"
 
 GECKO_URL="git://github.com/mozilla/releases-mozilla-central"
 GAIA_URL="git://github.com/mozilla-b2g/gaia"
@@ -66,12 +66,16 @@ build_device() {
 	cd $CURRENT_DIR
 	./add-commit.py $1 $out_dir/manifest.xml
 	./change_notes.py $CURRENT_DIR/out/$YESTERDAY/$2/manifest.xml $out_dir/manifest.xml $1 $out_dir
-	cp -rp $1/out/target/product/$2/*.img $out_dir
+	cp -rp $1/out/target/product/$2/system.img $out_dir
+	cp -rp $1/out/target/product/$2/userdata.img $out_dir
+	if [ -f "$1/out/target/product/$2/boot.img"]; then
+		cp -rp $1/out/target/product/$2/userdata.img $out_dir
+	fi
 	cp -rp flash/flash.sh $out_dir
 	mv $1/out $1/$TODAY
 }
 
-export ANDROIDFS_DIR=/home/wdeng/work/B2G-otoro/android_backup/otoro-ics-0727
+export ANDROIDFS_DIR=/home/wdeng/work/b2g-otoro/android_backup/otoro-ics-0727
 build_device $OTORO_SRC otoro
 
 #$1 dir name
@@ -97,7 +101,9 @@ build_sp8810ea() {
 	cd $CURRENT_DIR
 	./add-commit.py $SP8810EA_SRC $out_dir/manifest.xml
 	./change_notes.py $CURRENT_DIR/out/$YESTERDAY/sp8810ea/manifest.xml $out_dir/manifest.xml $SP8810EA_SRC $out_dir
-	cp -rp $SP8810EA_SRC/out/target/product/sp8810ea/*.img $out_dir
+	cp -rp $SP8810EA_SRC/out/target/product/sp8810ea/boot.img $out_dir
+	cp -rp $SP8810EA_SRC/out/target/product/sp8810ea/system.img $out_dir
+	cp -rp $SP8810EA_SRC/out/target/product/sp8810ea/userdata.img $out_dir
 	cp -rp flash/flash.sh $out_dir
 	mv $SP8810EA_SRC/out $SP8810EA_SRC/$TODAY
 }
